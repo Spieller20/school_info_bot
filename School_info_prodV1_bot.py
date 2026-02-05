cfff1d008882ff88fedb2a9012bd5d3de6c8aae0
Ivan Belov <GUF5518@yandex.ru>
2026-02-05 12:53:47 +0500

    School_info_prodV1_bot.py

--- School_info_prodV1_bot.py
+++ School_info_prodV1_bot.py
@@ -999,7 +999,7 @@ def func(message):
                       "<b><i>8 класс:</i></b>\n<b><i>Сольфеджио:</i></b> Класса нет"
                       "\n\n<b><i>Музыкальная литература:</i></b>\n Класса нет",
                          parse_mode='HTML')
-    with open('диктант до мажор 4 и 6 классы.m4a', 'rb') as auduo:    
+    with open('диктант до мажор 4 и 6 классы.m4a', 'rb') as audio:    
      bot.send_audio(message.chat.id, audio) 
     with open('диктант ре минор 5 класс.m4a', 'rb') as audio2:
      bot.send_audio(message.chat.id, audio2)
