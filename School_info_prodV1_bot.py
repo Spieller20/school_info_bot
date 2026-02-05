fe596d029f45140d389c8acefbf8ae5ace7b5a3e
Ivan Belov <GUF5518@yandex.ru>
2026-02-05 14:27:50 +0500

    School_info_prodV1_bot.py

--- School_info_prodV1_bot.py
+++ School_info_prodV1_bot.py
@@ -1003,9 +1003,9 @@ def func(message):
      audio_path = '/home/ubuntu/BotTG/диктант до мажор 4 и 6 классы.m4a'
      audio_path2 = '/home/ubuntu/BotTG/диктант ре минор 5 класс.m4a'
         with open(audio_path, 'rb') as audio:
-     bot.send_audio(message.chat.id, audio, caption='диктант до мажор 4 и 6 классы')            
+             bot.send_audio(message.chat.id, audio, caption='диктант до мажор 4 и 6 классы')            
         with open(audio_path2, 'rb') as audio2:
-     bot.send_audio(message.chat.id, audio2, caption='диктант ре минор 5 класс')
+             bot.send_audio(message.chat.id, audio2, caption='диктант ре минор 5 класс')
   
      bot.send_message(message.chat.id, "<b><i>1 кл. внебюджет, сольфеджио (7-9 лет):</i></b> Группа в 11:40:В учебнике номер 211 петь с дирижированием\n\nГруппа в 12:20: В учебнике номер 211 прохлопать ритм со счётом (на раз и два и три и четыре и)\n", parse_mode='HTML')
      bot.send_photo(message.chat.id, 'https://disk.yandex.ru/i/PvMDM9FlPC_4LQ', caption="<b><i>1 кл. внебюджет, сольфеджио (10-14 лет):</i></b> 1) рт. С. 31 номер 18\n2) подписать интервалы\n", parse_mode='HTML')
